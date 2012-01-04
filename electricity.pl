24acec79f19f0fa9c8dcd82e72a5b0a4e21ee94f
Severin Schols <severin@schols.de>
2012-01-04 11:59:01 +0100

    New configuration system, input sanity checks

--- electricity.pl
+++ electricity.pl
@@ -5,9 +5,7 @@ use HTTP::Request;
 use HTTP::Response;
 use LWP::UserAgent;
 
-use Data::Dumper;
-
-my $api_key = "INSERT_PACHUBE_API_KEY_HERE";
+my %config = do 'config.pl';
 
 my $input;
 my @values;
@@ -15,31 +13,35 @@ my $timestamp = 0;
 my $output;
 my $power;
 
-my $uri = 'http://api.pachube.com/v2/feeds/42055';
-my $req = HTTP::Request->new( 'PUT', $uri );
-$req->header( 'Content-Type' => 'application/json', 'X-PachubeApiKey' => $api_key );
+my $req = HTTP::Request->new( 'PUT', $config{pachube_feed_uri} );
+$req->header( 'Content-Type' => 'application/json', 'X-PachubeApiKey' => $confit{pachube_api_key} );
 my $lwp = LWP::UserAgent->new;
 my $response;
 
 while (1) {
+	# Get input from log file (the ugly way)
 	$input = `curl -r -60 http://blackbox.raumzeitlabor.de/strom/data/log 2>/dev/null| tail -n 2`;
-	@values = split(/[ \n\r]+/, $input);
 	
-	if ($timestamp != $values[2]) {
-		$power = ($values[3] - $values[1]) * 3600/($values[2] - $values[0]);
-		$timestamp = $values[0];
-		$output = "{
-	  \"version\":\"1.0.0\",
-	  \"datastreams\":[
-		  {\"id\":\"Strom_Leistung\", \"current_value\":\"" . $power . "\"},
-		  {\"id\":\"Strom_Gesamtverbrauch\", \"current_value\":\"" . $values[3]/1000 . "\"}
-	  ]
-	}\n";
-		#print $output;
-		$req->content( $output );
-		# print Dumper $req;
-		$response = $lwp->request( $req );
-		# print $response->status_line . "\n";
+	# Input Sanity Check
+	if ($input =~ m/^\d+\s\d+\s\r\n\d+\s\d+\s\r\n$/ ) {
+		@values = split(/[ \n\r]+/, $input);
+		
+		# Only update if new values found
+		if ($timestamp != $values[2]) {
+			$power = ($values[3] - $values[1]) * 3600/($values[2] - $values[0]);
+			$timestamp = $values[0];
+			$output = "{
+		  \"version\":\"1.0.0\",
+		  \"datastreams\":[
+			  {\"id\":\"Strom_Leistung\", \"current_value\":\"" . $power . "\"},
+			  {\"id\":\"Strom_Gesamtverbrauch\", \"current_value\":\"" . $values[3]/1000 . "\"}
+		  ]
+		}\n";
+			
+			# Send request to Pachube
+			$req->content( $output );
+			$response = $lwp->request( $req );
+		}
 	}
 	sleep(60);
 }
